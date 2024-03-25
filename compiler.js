a551cd757d11b46f371c924e49a74c572c4a1949
Ori <OriBennett@gmail.com>
2024-03-25 18:41:58 +0200

    first steps

---
+++ compiler.js
@@ -0,0 +1,12 @@
+const tokenizer = require('./tokenizer');
+
+module.exports = function compiler(input){
+  // 1. Lexical Analysis
+  // 2. Syntactic Analysis
+  // 3. Transformation
+  // 4. Code Generation
+  //
+  // return jsCode
+const tokens = tokenizer(input);
+
+}
---
+++ tokenizer.js
@@ -0,0 +1,10 @@
+module.exports = function tokenizer(input){
+    const tokens = [];
+    let current = 0;
+    while (current < input.length){
+        const char = input[current]
+        //bunch of if statements
+        throw new TypeError(`Unknown char '${char}'`);
+    }
+    return tokens;
+}
