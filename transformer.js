011978866a73467c3cd886367324162efdfbaebb
Ori Bennett <oribennett@gmail.com>
2024-03-26 19:28:42 +0200

    transformer done, need to implement traverse function

--- transformer.js
+++ transformer.js
@@ -1,8 +1,39 @@
+const traverse = require('./traverse');
 module.exports = function transformer(originalAST) {
   const jsAST = {
     type: "Program",
     body: [],
   };
+
+  let position = jsAST.body;
+
+  traverse(originalAST, {
+    NumberLiteral(node) {
+        position.push({ // <-- the position variable does not exist yet
+          type: 'NumericLiteral',
+          value: node.value
+        });
+      },
+      CallExpression(node, parent){
+        let expression = {
+            type: 'CallExpression',
+            calee: {
+                type: 'Identifier',
+                name: node.name
+            },
+            arguments: []
+        };
+        const prevPosition = position;
+        position = expression.arguments;
+        if (parent.type !== 'CallExpression'){
+            expression = {
+                type: 'ExpressionStatement',
+                expression
+            };
+        }
+        prevPosition.push(expression);
+      }
+  })
 
   return jsAST;
 };
